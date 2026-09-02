cf852b51c478ef9c6403ef7d9c5e4a3f71ea62a7
taeyoung son <ty.son@postech.ac.kr>
2026-09-02 22:06:48 +0900

    content: combine groom parents account

--- src/data/wedding.ts
+++ src/data/wedding.ts
@@ -101,8 +101,12 @@ export const galleryPhotos = [
 export const accounts = {
   groom: [
     { relation: '신랑', name: '손태영', bank: '우리은행', number: '1002-454-215893' },
-    { relation: '아버지', name: '손철호', bank: '국민은행', number: '832-24-0139-441' },
-    { relation: '어머니', name: '임성효', bank: '국민은행', number: '832-24-0139-441' },
+    {
+      relation: '부모님',
+      name: '손철호 · 임성효',
+      bank: '국민은행',
+      number: '832-24-0139-441',
+    },
   ],
   bride: [
     { relation: '신부', name: '조현아', bank: '신한은행', number: '110-497-717473' },
